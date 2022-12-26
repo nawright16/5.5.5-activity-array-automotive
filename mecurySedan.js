f08feed98582c0704edd8a46c8d3eacd4bd5909c
Nick Wright <nawright16@gmail.com>
2022-12-26 10:58:45 -0500

    extended Car from Vehicle, added constructor

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,10 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        
+    }
+}
